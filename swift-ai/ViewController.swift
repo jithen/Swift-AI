c983a3dbc4d25276f350e5e508093a3460c2fa1e
Jithendra Jonnareddi <jithendra.jonnareddi+farmers@farmersinsurance.com>
2026-01-20 09:10:26 -0800

    Initial setup

---
+++ swift-ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-ai
+//
+//  Created by Jithendra Jonnareddi on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift-aiTests/swift_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  swift_aiTests.swift
+//  swift-aiTests
+//
+//  Created by Jithendra Jonnareddi on 1/20/26.
+//
+
+import Testing
+@testable import swift_ai
+
+struct swift_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
